1c885073e45df50274cbffb50a13b22e7d3587d9
Danya Bee <bouskod8@cs.man.ac.uk>
2015-02-06 15:33:11 +0000

    Refactoring: extracting comma as a constant - in prep for next requirement

--- src/main/java/katas/stringCalculator/StringCalculator.java
+++ src/main/java/katas/stringCalculator/StringCalculator.java
@@ -3,11 +3,14 @@ package katas.stringCalculator;
 import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
+
+    private static final String DELIMITER = ",";
+
     public int add(String stringToAdd) {
         int total = 0;
 
         if (! stringToAdd.isEmpty()) {
-            String[] numbers = stringToAdd.split(",");
+            String[] numbers = stringToAdd.split(DELIMITER);
             for(String digit : numbers) {
                 total += parseInt(digit);
             }
