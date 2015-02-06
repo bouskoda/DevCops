2da0f60dcee2053de80e7c5deda0d179d649a3cf
Danya Bee <bouskod8@cs.man.ac.uk>
2015-02-06 15:24:21 +0000

    Refactoring: better tidy up of the main class

--- src/main/java/katas/stringCalculator/StringCalculator.java
+++ src/main/java/katas/stringCalculator/StringCalculator.java
@@ -6,13 +6,11 @@ public class StringCalculator {
     public int add(String stringToAdd) {
         int total = 0;
 
-        if (stringToAdd.isEmpty()) {
-            return total;
-        }
-
-        String[] numbers = stringToAdd.split(",");
-        for(String digit : numbers) {
-            total += parseInt(digit);
+        if (! stringToAdd.isEmpty()) {
+            String[] numbers = stringToAdd.split(",");
+            for(String digit : numbers) {
+                total += parseInt(digit);
+            }
         }
         return total;
     }
