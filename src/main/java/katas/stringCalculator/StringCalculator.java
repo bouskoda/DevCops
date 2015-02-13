65cf8deb39b12a4fb4fb0c6e265df93540dda00a
Danya Bee <bouskod8@cs.man.ac.uk>
2015-02-13 23:12:18 +0000

    Refactoring: attempted to make method names clearer. extracted out methods which were doing more than one thing

--- src/main/java/katas/stringCalculator/StringCalculator.java
+++ src/main/java/katas/stringCalculator/StringCalculator.java
@@ -51,13 +51,17 @@ public class StringCalculator {
         if (result.size() > 1) {
             String delimiter = result.get(0);
             String stringToSplit = result.get(1);
-            return extractAsInt(delimiter, stringToSplit);
+            return splitOn(delimiter, stringToSplit);
         }
-        return extractAsInt(DELIMITER, result.get(0));
+        return splitOn(DELIMITER, result.get(0));
     }
 
-    private int[] extractAsInt(String delimiter, String stringToSplit) {
+    private int[] splitOn(String delimiter, String stringToSplit) {
         String[] splitStrings = stringToSplit.split(delimiter);
+        return extractInt(splitStrings);
+    }
+
+    private int[] extractInt(String[] splitStrings) {
         int[] numbersToAdd = new int[splitStrings.length];
 
         for (int index = 0; index < numbersToAdd.length ; index++) {
