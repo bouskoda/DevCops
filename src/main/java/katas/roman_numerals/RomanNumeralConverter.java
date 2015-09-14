5f741a0bc3039c859715c0f3576d48338676f4ad
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-14 12:40:12 +0100

    RomanNumerals: refactored method for handling the numeral I

--- src/main/java/katas/roman_numerals/RomanNumeralConverter.java
+++ src/main/java/katas/roman_numerals/RomanNumeralConverter.java
@@ -2,12 +2,10 @@ package katas.roman_numerals;
 
 public class RomanNumeralConverter {
     public int convert(String romanNumeral) {
-        if (romanNumeral.equals("III")) {
-            return 3;
-        } else if (romanNumeral.equals("II")) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return convertMultiplesOfNumeralI(romanNumeral);
+    }
+
+    private int convertMultiplesOfNumeralI(String romanNumeral) {
+        return romanNumeral.length();
     }
 }
