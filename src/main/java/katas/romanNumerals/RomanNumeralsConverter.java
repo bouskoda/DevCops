9ab01f7fbc4af02eb566218db4ddbba4bfe0a636
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-28 22:46:08 +0100

    RomanNumerals: refactoring the code to perform additions more correctly than using string length

--- src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
+++ src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
@@ -3,13 +3,25 @@ package katas.romanNumerals;
 public class RomanNumeralsConverter {
 
     public int convert(String romanNumeral) {
+        String[] numerals = romanNumeral.split("");
+        int result = 0;
+        for (String numeral : numerals) {
+            result += evaluateNumeral(numeral);
+        }
+
+        return result;
+    }
+
+    private int evaluateNumeral(String romanNumeral) {
         switch (romanNumeral) {
             case "X":
                 return 10;
             case "V":
                 return 5;
+            case "I":
+                return 1;
             default:
-                return romanNumeral.length();
+                return 0;
         }
     }
 }
