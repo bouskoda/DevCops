bab3d4ba50beadec552ed6fa55b7f4f1aafddfde
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-28 20:03:38 +0100

    Revert "RomanNumerals: should have tidied up sooner"
    This reverts commit 84683c2c264db53a9dd84b81cffbf8d85b9738f3.

--- src/test/java/katas/roman_numerals/RomanNumeralConverterTest.java
+++ src/test/java/katas/roman_numerals/RomanNumeralConverterTest.java
@@ -7,20 +7,26 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class RomanNumeralConverterTest {
-    private RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
+    private RomanNumeralConverter romanNumeralConverter;
 
     @Test
     public void romanNumeralIConvertsToOne() {
+        romanNumeralConverter = new RomanNumeralConverter();
+
         assertThat(romanNumeralConverter.convert("I"), is(equalTo(1)));
     }
 
     @Test
     public void romanNumeralIIConvertsToTwo() {
+        romanNumeralConverter = new RomanNumeralConverter();
+
         assertThat(romanNumeralConverter.convert("II"), is(equalTo(2)));
     }
 
     @Test
     public void romanNumeralIIIConvertsToThree() {
+        romanNumeralConverter = new RomanNumeralConverter();
+
         assertThat(romanNumeralConverter.convert("III"), is(equalTo(3)));
     }
 }
