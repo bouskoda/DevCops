7a0d91a3ffcacf4baea673126e5cb21dab787263
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-29 14:56:41 +0100

    RomanNumerals: manual revert of II to two step

--- src/test/java/katas/romanNumerals/RomanNumeralsConverterTest.java
+++ src/test/java/katas/romanNumerals/RomanNumeralsConverterTest.java
@@ -3,8 +3,6 @@ package katas.romanNumerals;
 import org.junit.Test;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 public class RomanNumeralsConverterTest {
     private RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
@@ -18,17 +16,4 @@ public class RomanNumeralsConverterTest {
     public void romanNumeralIConvertsToOne() {
         assertThat(romanNumeralsConverter.convert("I")).isEqualTo(1);
     }
-
-    @Test
-    public void romanNumeralIIConvertsToTwo() {
-        assertThat(romanNumeralsConverter.convert("II")).isEqualTo(2);
-    }
-<<<<<<< HEAD
-
-    @Test
-    public void romanNumeralIIIConvertsToThree() {
-        assertThat(romanNumeralsConverter.convert("III")).isEqualTo(3);
-    }
-=======
->>>>>>> parent of b327c7b... RomanNumerals: adding test to confirm ability to convert III to three
 }
