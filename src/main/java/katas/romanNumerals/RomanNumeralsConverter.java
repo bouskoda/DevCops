d581a5d5ef861d5ec9a1d8f2a26f123784898bf3
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-29 13:24:00 +0100

    Revert "RomanNumerals: refactor step, introduced switch to handle the characters"
    This reverts commit c8fc71e0976d4bedeb863390cf1e3e9f36e5ac6b.

--- src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
+++ src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
@@ -3,13 +3,12 @@ package katas.romanNumerals;
 public class RomanNumeralsConverter {
 
     public int convert(String romanNumeral) {
-        switch (romanNumeral) {
-            case "X":
-                return 10;
-            case "V":
-                return 5;
-            default:
-                return romanNumeral.length();
+        if (romanNumeral.equals("X")) {
+            return 10;
+        } else if (romanNumeral.equals("V")) {
+            return 5;
+        } else {
+            return romanNumeral.length();
         }
     }
 }
