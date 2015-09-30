e186a1c7b62e84697a422d1303747de933c48fd0
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-30 14:35:31 +0100

    RomanNumerals: ability to convert I to one. TPP5 Unconditional->Conditional

--- src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
+++ src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
@@ -3,6 +3,9 @@ package katas.romanNumerals;
 public class RomanNumeralsConverter {
 
     public int convert(String romanNumeral) {
-        return 0;
+        if (romanNumeral.isEmpty()) {
+            return 0;
+        }
+        return 1;
     }
 }
