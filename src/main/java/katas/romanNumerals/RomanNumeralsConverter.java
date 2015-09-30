fa71e4cd579041b588cf44c09aa8385d31e83ca2
Danya Bee <bouskod8@cs.man.ac.uk>
2015-09-30 14:30:11 +0100

    RomanNumerals: back again to the very first test case, empty string

--- src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
+++ src/main/java/katas/romanNumerals/RomanNumeralsConverter.java
@@ -1,32 +1,8 @@
 package katas.romanNumerals;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RomanNumeralsConverter {
 
-    private static Map<String, Integer> results = new HashMap<>();
-    static {
-        results.put("", 0);
-        results.put("I", 1);
-        results.put("IV", 4);
-        results.put("V", 5);
-        results.put("IX", 9);
-        results.put("X", 10);
-        results.put("XL", 40);
-        results.put("L", 50);
-        results.put("XC", 90);
-        results.put("C", 100);
-        results.put("CD", 400);
-        results.put("D", 500);
-    }
-
     public int convert(String romanNumeral) {
-        if (results.containsKey(romanNumeral)) {
-            return results.get(romanNumeral);
-        }
-
-        return results.get(romanNumeral.substring(0,1))
-                + convert(romanNumeral.substring(1));
+        return 0;
     }
 }
--- src/test/java/katas/romanNumerals/RomanNumeralsConverterTest.java
+++ src/test/java/katas/romanNumerals/RomanNumeralsConverterTest.java
@@ -1,39 +1,14 @@
 package katas.romanNumerals;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
-@RunWith(JUnitParamsRunner.class)
 public class RomanNumeralsConverterTest {
 
-    private RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
-
     @Test
-    @Parameters({", 0",
-                "I, 1",
-                "II, 2",
-                "III, 3",
-                "IV, 4",
-                "V, 5",
-                "VI, 6",
-                "VII, 7",
-                "VIII, 8",
-                "IX, 9",
-                "X, 10",
-                "XXX, 30",
-                "XL, 40",
-                "L, 50",
-                "LXX, 70",
-                "XC, 90",
-                "C, 100",
-                "CL, 150",
-                "CD, 400",
-                "D, 500"})
-    public void romanNumeralConvertsToExpectedArabicNumber(String romanNumeral, int expectedNumber) {
-        assertThat(romanNumeralsConverter.convert(romanNumeral)).isEqualTo(expectedNumber);
+    public void emptyStringConvertsToZero() {
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+        assertThat(romanNumeralsConverter.convert("")).isEqualTo(0);
     }
 }
