a913e03ac62bba0436a17303ed62a4262becd587
Danya Bee <bouskod8@cs.man.ac.uk>
2015-10-20 20:29:37 +0100

    [BrutalRefactoring] Fixing failing GridTest

--- src/main/java/katas/brutalRefactoring/Grid.java
+++ src/main/java/katas/brutalRefactoring/Grid.java
@@ -1,13 +1,18 @@
 package katas.brutalRefactoring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Grid {
 
+    private Map<GridPosition, Player> GRID = new HashMap<>();
+
 	public void addMove(Player player, GridPosition gridPosition) {
-		
+		GRID.put(gridPosition,player);
 	}
 
 	public Player getPlayerAtMove(GridPosition gridPosition) {
-		return null;
+		return GRID.get(gridPosition);
 	}
 
 }
